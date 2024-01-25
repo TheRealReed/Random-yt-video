3d1af2f35390132b4775bc0f9c2781419785879b
TheRealReed <143298312+TheRealReed@users.noreply.github.com>
2024-01-24 18:58:37 -0600

    Update pip3install.bat

--- pip3install.bat
+++ pip3install.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Make sure you have pip3 and Python installed!!
-pip install requests
-pip install re
-pip install random
+pip3 install requests
+pip3 install re
+pip3 install random
 echo its funny how they all start with "r"
 pause
